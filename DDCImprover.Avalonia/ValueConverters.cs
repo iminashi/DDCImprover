a6b9ac00ba63a2603b3c3e0712ccde90e244fdf1
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-05-27 13:32:35 +0300

    Improvements to the Avalonia UI
    Use icons from Material Design instead of image files. The icons on the tool bar buttons are now grayed out when disabled.
    Change the color of the progress bar.

---
+++ DDCImprover.Avalonia/ValueConverters.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+using System.Linq;
+
+namespace DDCImprover.Avalonia
+{
+    public static class ValueConverters
+    {
+        public static readonly IMultiValueConverter IsEnabledToBrush = new FuncMultiValueConverter<object, IBrush>(
+        v =>
+        {
+            var values = v.ToArray();
+            if (values[0] is bool isEnabled && isEnabled && values[1] is string colorStr)
+            {
+                return Brush.Parse(colorStr);
+            }
+
+            return Brushes.Gray;
+        });
+    }
+}
